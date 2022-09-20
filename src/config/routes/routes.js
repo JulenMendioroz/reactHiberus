fd3b171622697d21c4acf77c60e58f2140d89e43
Julen Mendioroz <jmendiorozredin@gmail.com>
2022-09-20 21:39:02 +0200

    FIX routes config for deploy

--- src/config/routes/routes.js
+++ src/config/routes/routes.js
@@ -1,11 +1,11 @@
-const BASE_ROUTE = "/reactHiberus"
+const BASE_ROUTE = "/reactHiberus/"
 
 export const routes = {
   HOME: `${BASE_ROUTE}`,
-  PERSONAJES: `${BASE_ROUTE}/personajes`,
-  PERSONAJE: `${BASE_ROUTE}/personajes/:id`,
-  EPISODIOS: `${BASE_ROUTE}/episodios`,
-  EPISODIO: `${BASE_ROUTE}/episodios/:id`,
-  CONTACTO: `${BASE_ROUTE}/contacto`,
-  JUEGO: `${BASE_ROUTE}/juego`,
+  PERSONAJES: `${BASE_ROUTE}personajes`,
+  PERSONAJE: `${BASE_ROUTE}personajes/:id`,
+  EPISODIOS: `${BASE_ROUTE}episodios`,
+  EPISODIO: `${BASE_ROUTE}episodios/:id`,
+  CONTACTO: `${BASE_ROUTE}contacto`,
+  JUEGO: `${BASE_ROUTE}juego`,
 }
